be208a9dbd58c404e585ce23d851f156e4517135
Suryansh Singh <nishusinghjbp@gmail.com>
2020-05-24 23:39:13 +0530

    Codeforces Round 644 Div 3 Cntest

---
+++ Codeforces/644Div3/D/sol.cpp
@@ -0,0 +1,127 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define present(c,x) (c.find(x) != c.end())
+#define cpresent(c,x) (find(all(c),x) != c.end())
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+int mpow(int base, int exp); 
+void ipgraph(int m);
+void dfs(int u, int par);
+const int mod = 1000000007;
+const int N = 3e5, M = N;
+//=======================
+
+vi g[N];
+vi vec;
+int a[N];
+void SieveOfEratosthenes(int n) 
+{ 
+    bool prime[n+1];
+
+    memset(prime, true, sizeof(prime)); 
+  
+    for (int p=2; p*p<=n; p++) 
+    { 
+        if (prime[p] == true) 
+        { 
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+  
+    for (int p=2; p<=n; p++) 
+       if (prime[p]) 
+          vec.push_back(p); 
+} 
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    ll int t, i, j, k, p, q, r=0, x, y, u, v, n, m;
+    long double first,last,mid;
+    cin>>t;
+    fo(i,t){
+      cin>>n>>k;
+      SieveOfEratosthenes(100000);
+      for(j=0;j<vec.size();j++){
+        if(vec.at(j) == n){
+          cout<<n<<endl;
+          r=1;
+          break;
+        }
+      }
+      if(r)
+        continue;
+      if(k>n || k==n){
+        cout<<1<<endl;
+      }
+      else{
+        j=2;
+        while(true){
+          if(k>=n/j && n%j==0){    
+            cout<<j<<endl;
+            break;
+          }
+          else{
+            j++;
+          }
+        }
+      }
+    }
+    return 0;
+}
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+void ipgraph(int n, int m){
+	int i, u, v;
+	while(m--){
+		cin>>u>>v;
+		g[u-1].pb(v-1);
+		g[v-1].pb(u-1);
+	}
+}
+
+void dfs(int u, int par){
+	for(int v:g[u]){
+		if (v == par) continue;
+		dfs(v, u);
+	}
+}
+
+
